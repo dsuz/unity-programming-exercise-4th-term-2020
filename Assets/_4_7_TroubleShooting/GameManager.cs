4e4d042fa6900c279b93dc989662a83e33443518
D. Suzuki <bboydaisuke@gmail.com>
2021-02-07 20:56:17 +0900

    7週目用のシューティングゲームを追加した

---
+++ Assets/_4_7_TroubleShooting/GameManager.cs
@@ -0,0 +1,138 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+
+/// <summary>
+/// ゲームを管理する。適当なオブジェクトにアタッチし、各種設定をすれば動作する。
+/// </summary>
+public class GameManager : MonoBehaviour
+{
+    /// <summary>Windows のマウスカーソルをゲーム中に消すかどうかの設定</summary>
+    [SerializeField] bool m_hideSystemMouseCursor = false;
+    /// <summary>敵オブジェクトがいるレイヤー</summary>
+    [SerializeField] LayerMask m_enemyLayer;
+    /// <summary>照準の Image (UI)</summary>
+    [SerializeField] Image m_crosshairImage = null;
+    /// <summary>照準に敵が入っていない時の色</summary>
+    [SerializeField] Color m_colorNormal = Color.white;
+    /// <summary>照準に敵が入っている時の色</summary>
+    [SerializeField] Color m_colorFocus = Color.red;
+    /// <summary>銃のオブジェクト</summary>
+    [SerializeField] GameObject m_gunObject = null;
+    /// <summary>銃の操作のために Ray を飛ばす距離</summary>
+    [SerializeField] float m_rangeDistance = 100f;
+    /// <summary>現在照準で狙われている敵</summary>
+    GunEnemyController m_currentTargetEnemy = null;
+    /// <summary>発砲した時の効果音</summary>
+    [SerializeField] AudioClip m_gunFireSfx = null;
+    AudioSource m_audio = null;
+    int m_score = 0;
+    /// <summary>スコアを表示するための Text (UI)</summary>
+    [SerializeField] Text m_scoreText = null;
+    /// <summary>ライフの初期値</summary>
+    [SerializeField] int m_initialLife = 500;
+    /// <summary>ライフの現在値</summary>
+    int m_life;
+    /// <summary>ライフを表示するための Text (UI)</summary>
+    [SerializeField] Text m_lifeText = null;
+    /// <summary>カメラを揺らす設定</summary>
+    [SerializeField] Cinemachine.CinemachineImpulseSource m_cameraShakeSource = null;
+    /// <summary>ダメージを受けた時に赤くして表現するためのパネル</summary>
+    [SerializeField] Animator m_hitScreen = null;
+    /// <summary>全ての敵オブジェクトを入れておくための List</summary>
+    List<GunEnemyController> m_enemies = null;
+    /// <summary>ゲームオーバー時に表示するパネル</summary>
+    [SerializeField] Animator m_gameoverPanel = null;
+
+    void Start()
+    {
+        if (m_hideSystemMouseCursor)
+        {
+            Cursor.visible = false;
+        }
+
+        m_life = m_initialLife;
+        m_score = 0;
+        m_audio = GetComponent<AudioSource>();
+        m_enemies = GameObject.FindObjectsOfType<GunEnemyController>().ToList();
+        m_lifeText.text = string.Format("{0:000}", m_life);
+        m_scoreText.text = string.Format("{0:0000000000}", m_score);
+        m_gameoverPanel.gameObject.SetActive(false);
+    }
+
+    /// <summary>
+    /// ゲームをやり直す
+    /// </summary>
+    public void Restart()
+    {
+        Debug.Log("Restart");
+        m_enemies.ForEach(enemy => enemy.gameObject.SetActive(true));   // LINQ
+        Start();
+    }
+
+    /// <summary>
+    /// ゲームオーバー
+    /// </summary>
+    void Gameover()
+    {
+        m_gameoverPanel.gameObject.SetActive(true);
+        m_gameoverPanel.Play("Show");
+        m_enemies.ForEach(enemy => enemy.gameObject.SetActive(false));  // LINQ
+    }
+
+    void Update()
+    {
+        m_crosshairImage.rectTransform.position = Input.mousePosition;
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, m_rangeDistance))
+        {
+            m_gunObject.transform.LookAt(hit.point);    // 銃の方向を変えている
+        }
+
+        // 敵が照準に入っているか調べる
+        bool isEnemyTargeted = Physics.Raycast(ray, out hit, m_rangeDistance, m_enemyLayer);
+        m_crosshairImage.color = isEnemyTargeted ? m_colorFocus : m_colorNormal;
+        m_currentTargetEnemy = isEnemyTargeted ? hit.collider.gameObject.GetComponent<GunEnemyController>() : null;
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            // カメラを揺らして効果音を鳴らす
+            m_cameraShakeSource.GenerateImpulse();
+            m_audio.clip = m_gunFireSfx;
+            m_audio.Play();
+
+            // 敵に当たったら得点を足して表示を更新する
+            if (m_currentTargetEnemy)
+            {
+                m_score += m_currentTargetEnemy.Hit();
+                m_scoreText.text = string.Format("{0:0000000000}", m_score);
+            }
+        }
+    }
+
+    private void OnApplicationQuit()
+    {
+        Cursor.visible = true;
+    }
+
+    /// <summary>
+    /// 攻撃を食らった時に呼ぶ
+    /// </summary>
+    public void Hit()
+    {
+        // ライフを減らして表示を更新し、エフェクトを再生する
+        m_life -= 1;
+        m_lifeText.text = string.Format("{0:000}", m_life);
+        m_hitScreen.SetTrigger("Hit");
+        
+        if (m_life < 1)
+        {
+            Gameover();
+        }
+    }
+}
---
+++ Assets/_4_7_TroubleShooting/GunEnemyController.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 敵を制御するコンポーネント。敵は独立して動いているため、プレハブを増やせば敵を増やすことができる。
+/// ただし、m_onShoot だけはプレハブを置いた後に設定しなければならないため、動的に敵を生成する場合は m_onShoot を設定する処理をすること。
+/// </summary>
+public class GunEnemyController : MonoBehaviour
+{
+    /// <summary>Idle 状態から Ready 状態に移る時間の最小値（単位: 秒）</summary>
+    [SerializeField] float m_minInterval = 1f;
+    /// <summary>Idle 状態から Ready 状態に移る時間の最大値（単位: 秒）</summary>
+    [SerializeField] float m_maxInterval = 5f;
+    /// <summary>Ready 状態から攻撃までの時間の最小値（単位: 秒）</summary>
+    [SerializeField] float m_minFireTime = 0.1f;
+    /// <summary>Ready 状態から攻撃までの時間の最大値（単位: 秒）</summary>
+    [SerializeField] float m_maxFireTime = 0.3f;
+    /// <summary>キャラクターアニメーションのための Animator</summary>
+    [SerializeField] Animator m_animator = null;
+    /// <summary>攻撃が当たった時に加算される点</summary>
+    [SerializeField] int m_score = 100;
+    /// <summary>攻撃時の SFX</summary>
+    [SerializeField] AudioClip m_shotSfx = null;
+    /// <summary>キャラクターの状態</summary>
+    GunEnemyStatus m_status = GunEnemyStatus.Idle;
+    /// <summary>タイマー</summary>
+    float m_timer;
+    /// <summary>Idle 状態から Ready 状態に移るまでの時間（単位: 秒）</summary>
+    float m_interval;
+    /// <summary>Ready 状態から攻撃までの時間（単位: 秒）</summary>
+    float m_fireTime;
+    /// <summary>攻撃判定のためのコライダー</summary>
+    Collider m_collider = null;             // Collider は BoxCollider, SphereCollider などの基底クラスである
+    /// <summary>敵が銃を撃った時に実行される処理</summary>
+    public UnityEngine.Events.UnityEvent m_onShoot = null;
+
+    /// <summary>
+    /// 初期化処理
+    /// </summary>
+    void OnEnable()
+    {
+        this.transform.LookAt(Camera.main.transform.position);
+        m_collider = GetComponent<Collider>();
+        m_collider.enabled = false;
+        m_status = GunEnemyStatus.Idle;
+        ResetTimer();
+    }
+
+    void Update()
+    {
+        m_timer += Time.deltaTime;
+
+        switch (m_status)
+        {
+            case GunEnemyStatus.Idle:
+                if (m_timer > m_interval)
+                {
+                    ResetTimer();
+                    m_status = GunEnemyStatus.Ready;
+                    m_animator.SetTrigger("Ready");
+                    m_collider.enabled = true;
+                }
+                break;
+            case GunEnemyStatus.Ready:
+                if (m_timer > m_fireTime)
+                {
+                    ResetTimer();
+                    m_status = GunEnemyStatus.Idle;
+                    m_animator.SetTrigger("Fire");
+                    m_collider.enabled = false;
+                    AudioSource.PlayClipAtPoint(m_shotSfx, this.transform.position);
+                    
+                    if (m_onShoot != null)
+                    {
+                        m_onShoot.Invoke();
+                    }
+                    else
+                    {
+                        Debug.LogWarning("発砲時に呼ばれる処理が指定されていません。");
+                    }
+                }
+                break;
+        }
+    }
+
+    /// <summary>
+    /// タイマーをリセットする
+    /// </summary>
+    void ResetTimer()
+    {
+        m_timer = 0;
+        m_fireTime = Random.Range(m_minFireTime, m_maxFireTime);    // Idle -> Ready までの時間をランダムに決める
+        m_interval = Random.Range(m_minInterval, m_maxInterval);    // Ready -> 攻撃までの時間をランダムに決める
+    }
+
+    /// <summary>
+    /// プレイヤーの攻撃が当たった時に呼ぶ
+    /// Idle 状態（初期状態）に戻す
+    /// </summary>
+    /// <returns></returns>
+    public int Hit()
+    {
+        ResetTimer();
+        m_status = GunEnemyStatus.Idle;
+        m_animator.SetTrigger("Hit");
+        m_collider.enabled = false;
+        return m_score;
+    }
+}
+
+/// <summary>
+/// 敵の状態を表す
+/// </summary>
+enum GunEnemyStatus
+{
+    /// <summary>壁の裏に隠れて待機している状態。こちらの攻撃は当たらない
+    /// </summary>
+    Idle,
+    /// <summary>銃を構えてこちらを狙っている状態。この時にこちらの攻撃が当たる。</summary>
+    Ready,
+}
