4caef4a8e9203b4307466648841fad1beaec7f66
D. Suzuki <bboydaisuke@gmail.com>
2021-02-09 21:07:27 +0900

    課題のためにバグを混入させた

--- Assets/_4_7_TroubleShooting/GameManager.cs
+++ Assets/_4_7_TroubleShooting/GameManager.cs
@@ -43,20 +43,26 @@ public class GameManager : MonoBehaviour
     List<GunEnemyController> m_enemies = null;
     /// <summary>現在照準で狙われている敵</summary>
     GunEnemyController m_currentTargetEnemy = null;
+    /// <summary>ライフを表示するための GameObject</summary>
+    GameObject m_lifeObject;
 
     void Start()
     {
+        m_onGameStart.Invoke();
+
+        m_life = m_initialLife;
+        // m_score = 0;
+        m_enemies = GameObject.FindObjectsOfType<GunEnemyController>().ToList();    // LINQ を使うために配列ではなく List に保存する
+        m_lifeObject = GameObject.Find("LifeText");
+        m_lifeText = m_lifeObject.GetComponent<Text>();
+        m_lifeText.text = string.Format("{0:000}", m_life);
+        // m_scoreText.text = string.Format("{0:0000000000}", m_score);
+
         if (m_hideSystemMouseCursor)
         {
             Cursor.visible = false;
         }
 
-        m_life = m_initialLife;
-        // m_score = 0;
-        m_enemies = GameObject.FindObjectsOfType<GunEnemyController>().ToList();    // LINQ を使うために配列ではなく List に保存する
-        m_lifeText.text = string.Format("{0:000}", m_life);
-        // m_scoreText.text = string.Format("{0:0000000000}", m_score);
-        m_onGameStart.Invoke();
     }
 
     /// <summary>
